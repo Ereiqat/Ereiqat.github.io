49b2aa455962ace3b7724b5555c4424ad2a41075
Anas Ereiqat <113611140+Ereiqat@users.noreply.github.com>
2023-08-06 17:51:20 +0300

    Add files via upload

---
+++ responsive.js
@@ -0,0 +1,72 @@
+//multi item slider
+const preBtn = document.querySelector('.btn-pre');
+const nxtBtn = document.querySelector('.btn-nxt');
+
+const projContainer = document.querySelector('.proj-container');
+
+const slideAmount = 400; 
+
+const slidePrevious = () => {
+  projContainer.scrollBy(-slideAmount, 0);
+};
+
+const slideNext = () => {
+  projContainer.scrollBy(slideAmount, 0);
+};
+
+preBtn.addEventListener('click', slidePrevious);
+nxtBtn.addEventListener('click', slideNext);
+//end multi item slider
+
+//contact copy to clipboard
+const myphone = "0785855449";
+const myEmail = "ibr20200491@std.psut.edu.jo";
+
+var copy1 = document.getElementById('copy1');
+var copy2 = document.getElementById('copy2');
+
+function copyNumber(){
+  navigator.clipboard.writeText(myphone);
+  copy1.innerHTML = "Copied!";
+  copy2.innerHTML = "Copy";
+}
+
+function copyEmail(){
+  navigator.clipboard.writeText(myEmail);
+  copy2.innerHTML = "Copied!";
+  copy1.innerHTML = "Copy";
+}
+
+function btnReset(){
+  copy2.innerHTML = "Copy";
+  copy1.innerHTML = "Copy"
+}
+//end contact copy to clipboard
+
+//scroll to top
+let mybutton = document.querySelector(".topScroll");
+
+window.onscroll = function() {toggleBtnVisiblity()};
+
+function toggleBtnVisiblity() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function goTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+mybutton.addEventListener("click",goTop);
+//end scroll to top
+
+//scroll to About
+function scrollAbout(){
+  let aboutDiv = document.getElementById("About");
+  aboutDiv.scrollIntoView()
+}
+//end scroll to About
---
+++ responsive2.js
@@ -0,0 +1,84 @@
+//carousel
+const buttons = document.querySelectorAll("[data-carousell-button]")
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carousellButton === "next" ? 1 : -1
+    const sllides = button
+      .closest("[data-carousell]")
+      .querySelector("[data-sllides]")
+
+    const activeSllide = sllides.querySelector("[data-active]")
+    let newIndex = [...sllides.children].indexOf(activeSllide) + offset
+    if (newIndex < 0) newIndex = sllides.children.length - 1
+    if (newIndex >= sllides.children.length) newIndex = 0
+
+    sllides.children[newIndex].dataset.active = true
+    delete activeSllide.dataset.active
+  })
+})
+//end carousel
+
+autoSlide();
+
+function autoSlide() {
+    const offset = 1
+    const sllides = buttons[0].closest("[data-carousell]")
+      .querySelector("[data-sllides]")
+
+    const activeSllide = sllides.querySelector("[data-active]")
+    let newIndex = [...sllides.children].indexOf(activeSllide) + offset
+    if (newIndex >= sllides.children.length) newIndex = 0
+
+    sllides.children[newIndex].dataset.active = true
+    delete activeSllide.dataset.active
+
+    setTimeout(autoSlide, 5000);
+  }
+
+
+//contact copy to clipboard
+const myphone = "0785855449";
+const myEmail = "ibr20200491@std.psut.edu.jo";
+
+var copy1 = document.getElementById('copy1');
+var copy2 = document.getElementById('copy2');
+
+function copyNumber(){
+  navigator.clipboard.writeText(myphone);
+  copy1.innerHTML = "Copied!";
+  copy2.innerHTML = "Copy";
+}
+
+function copyEmail(){
+  navigator.clipboard.writeText(myEmail);
+  copy2.innerHTML = "Copied!";
+  copy1.innerHTML = "Copy";
+}
+
+function btnReset(){
+  copy2.innerHTML = "Copy";
+  copy1.innerHTML = "Copy"
+}
+//end contact copy to clipboard
+
+//scroll to top
+let mybutton = document.querySelector(".topScroll");
+
+window.onscroll = function() {toggleBtnVisiblity()};
+
+function toggleBtnVisiblity() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function goTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+mybutton.addEventListener("click",goTop);
+//end scroll to top
