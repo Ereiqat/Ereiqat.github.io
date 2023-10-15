e0fa556224e7cb2f8469d1652dc15ef97a974a06
IBRASANN <ibrahim.sanna491@gmail.com>
2023-10-15 12:00:21 +0300

    -some colors have been changed -font family changed to outfit -width of .about divs changed -line height & word spacing changed -pre and next buttons on project slider disappear in small devices -back to top button modified to look better on small devices

--- responsive2.js
+++ responsive2.js
@@ -97,7 +97,7 @@ window.onscroll = function() {toggleBtnVisiblity()};
 
 function toggleBtnVisiblity() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    mybutton.style.display = "flex";
   } else {
     mybutton.style.display = "none";
   }
