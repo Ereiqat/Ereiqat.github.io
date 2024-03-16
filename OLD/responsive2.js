5b466d3504fdfe32cfd94158142b933572b57316
AnasEreiqat <anaselham@hotmail.com>
2024-03-16 22:27:17 +0300

    New Site #1

---
+++ OLD/responsive2.js
@@ -0,0 +1,112 @@
+//carousel
+const buttons = document.querySelectorAll("[data-carousell-button]")
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carousellButton === "next" ? 1 : -1
+    const sllides = button
+      .closest("[data-carousell]")
+      .querySelector("[data-sllides]")
+
+
+    const activeSllide = sllides.querySelector("[data-active]")
+    let newIndex = [...sllides.children].indexOf(activeSllide) + offset
+    if (newIndex < 0) newIndex = sllides.children.length - 1
+    if (newIndex >= sllides.children.length) newIndex = 0
+
+    sllides.children[newIndex].dataset.active = true
+    delete activeSllide.dataset.active
+  })
+})
+
+const btnAuto = document.querySelectorAll(".imgCaro>.carousell-button");
+
+autoSlide();
+
+
+
+function autoSlide() {
+    const offset = 1
+    const sllides = btnAuto[0].closest("[data-carousell]")
+      .querySelector("[data-sllides]")
+
+    const activeSllide = sllides.querySelector("[data-active]")
+    let newIndex = [...sllides.children].indexOf(activeSllide) + offset
+    if (newIndex >= sllides.children.length) newIndex = 0
+
+    sllides.children[newIndex].dataset.active = true
+    delete activeSllide.dataset.active
+
+    setTimeout(autoSlide, 5000);
+  }
+
+const btnVid = document.querySelectorAll(".vidCaro>.carousell-button");
+
+btnVid.forEach(button => {
+  button.addEventListener("click", () => {
+    const sllides = button.closest("[data-carousell]")
+      .querySelector("[data-sllides]")
+
+    const offset = button.dataset.carousellButton === "next" ? -1 : 1
+
+    const activeSllide = sllides.querySelector("[data-active]")
+    let index = [...sllides.children].indexOf(activeSllide)
+
+    let vids = document.querySelectorAll(".vidCaro video")
+    if((index+offset)<0||(index+offset)>sllides.children.length - 1){
+    vids[0].pause()
+    vids[sllides.children.length - 1].pause()
+    }
+    else{
+    vids[index+offset].pause()
+    }
+    vids[index].play()
+  })
+})
+//end carousel
+
+//contact copy to clipboard
+const myphone = document.getElementById("phoneNum").innerHTML.toString();
+const myEmail = document.getElementById("email").innerHTML.toString();
+
+var copy1 = document.getElementById('copy1');
+var copy2 = document.getElementById('copy2');
+
+function copyNumber(){
+  navigator.clipboard.writeText(myphone);
+  copy1.innerHTML = "Copied!";
+  copy2.innerHTML = "Copy";
+}
+
+function copyEmail(){
+  navigator.clipboard.writeText(myEmail);
+  copy2.innerHTML = "Copied!";
+  copy1.innerHTML = "Copy";
+}
+
+function btnReset(){
+  copy2.innerHTML = "Copy";
+  copy1.innerHTML = "Copy"
+}
+//end contact copy to clipboard
+
+//scroll to top
+let mybutton = document.querySelector(".topScroll");
+
+window.onscroll = function() {toggleBtnVisiblity()};
+
+function toggleBtnVisiblity() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function goTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+mybutton.addEventListener("click",goTop);
+//end scroll to top
--- README.md
+++
@@ -1,1 +0,0 @@
-https://ereiqat.me
